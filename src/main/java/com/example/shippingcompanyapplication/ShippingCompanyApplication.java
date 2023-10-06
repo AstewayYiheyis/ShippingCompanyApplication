860a967b86d145cbfe9fc39ee328231070ba1a46
Asteway <90155375+AstewayYiheyis@users.noreply.github.com>
2023-10-06 11:51:21 -0900

    enabling swagger

--- src/main/java/com/example/shippingcompanyapplication/ShippingCompanyApplication.java
+++ src/main/java/com/example/shippingcompanyapplication/ShippingCompanyApplication.java
@@ -2,10 +2,10 @@ package com.example.shippingcompanyapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ShippingCompanyApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShippingCompanyApplication.class, args);
